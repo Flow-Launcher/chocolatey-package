8803097fe35962329af59313b8b74670c131edf6
Jeremy Wu <jeremy24wu@gmail.com>
2022-02-25 08:30:15 +1100

    Update chocolateyinstall.ps1

--- flow-launcher/tools/chocolateyinstall.ps1
+++ flow-launcher/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/Flow-Launcher/Flow.Launcher/releases/download/v1.9.0/Flow-Launcher-Setup.exe' 
 
-if ([Environment]::Is64BitOperatingSystem) {
+if (![Environment]::Is64BitOperatingSystem) {
     throw "This package requires 64 bit OS architecture."
 }
 
