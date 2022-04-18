d8d34b07f4ab742191fdbeff29bab7810d076459
Jeremy <jeremy24wu@gmail.com>
2022-04-18 12:11:36 +1000

    remove obsolete 64 bit check

--- flow-launcher/tools/chocolateyinstall.ps1
+++ flow-launcher/tools/chocolateyinstall.ps1
@@ -2,10 +2,6 @@ $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64bit   = 'https://github.com/Flow-Launcher/Flow.Launcher/releases/download/v1.9.0/Flow-Launcher-Setup.exe' 
 
-if (![Environment]::Is64BitOperatingSystem) {
-    throw "This package requires 64 bit OS architecture."
-}
-
 $packageArgs = @{
   packageName     = $env:ChocolateyPackageName
   unzipLocation   = $toolsDir
